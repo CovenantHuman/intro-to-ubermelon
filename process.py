fa53f49e07d1d7d3c3ed2d7c906a8ad0f2ff5a89
Michaela Healton <mshealton@gmail.com>
2022-08-09 17:16:14 -0700

    Changed report to Monday sales

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ def generate_sales_reports(log_file): #creates a function to interact with the f
     for line in log_file: #loops through contents of the file one line at a time
         line = line.rstrip() #removes the unneeded spaces on the right side of each line of the file
         day = line[0:3] #saves the first three characters in each line to a variable
-        if day == "Tue": #checks if the variable created in the line above indicates Tuesday
+        if day == "Mon": #checks if the variable created in the line above indicates Monday
             print(line) #adds a line to the report
 
 
